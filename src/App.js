baac5828833220f7e2fa52d8e34f517ac88ce646
zislamtanvir <zislamtanvir@gmail.com>
2022-12-13 10:40:09 +0600

    Total feedback shown after all

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ const App = () => {
     const [good, setGood] = useState(0);
     const [netural, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-    // const [totalFeedback, setToalFeedback] = useState(0);
+    const [totalFeedback, setToalFeedback] = useState(0);
 
     const incrementGood = () => {
         setGood(good + 1);
-        // setToalFeedback(totalFeedback + 1);
+        setToalFeedback(totalFeedback + 1);
     };
     const incrementNetual = () => {
         setNeutral(netural + 1);
-        // setToalFeedback(totalFeedback + 1);
+        setToalFeedback(totalFeedback + 1);
     };
     const badIncrement = () => {
         setBad(bad + 1);
-        // setToalFeedback(totalFeedback + 1);
+        setToalFeedback(totalFeedback + 1);
     };
     return (
         <React.Fragment>
@@ -33,7 +33,7 @@ const App = () => {
             <p>good {good}</p>
             <p>netural {netural}</p>
             <p>bad {bad}</p>
-            {/* <p>all {totalFeedback}</p> */}
+            <p>all {totalFeedback}</p>
         </React.Fragment>
     );
 };
